d547d19a2e62729a01bc5c6c73aa7fd3ba219201
jellero <j.ellero@gmail.com>
2026-07-15 22:10:47 +0200

    Centralizza la versione schema attesa

---
+++ app/Core/Database/SchemaVersion.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hapa\Core\Database;
+
+final class SchemaVersion
+{
+    public const LATEST = '20260715201500';
+
+    private function __construct()
+    {
+    }
+}
