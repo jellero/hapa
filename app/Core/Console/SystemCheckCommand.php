0d33dcf3db1b4677d318289774c0f5b4406fc34d
jellero <j.ellero@gmail.com>
2026-07-15 23:08:04 +0200

    Inietta readiness nel comando di sistema

--- app/Core/Console/SystemCheckCommand.php
+++ app/Core/Console/SystemCheckCommand.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Hapa\Core\Console;
 
-use Hapa\Core\Database\ConnectionFactory;
 use Hapa\Core\Health\ReadinessCheck;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -14,9 +13,14 @@ use Symfony\Component\Console\Output\OutputInterface;
 #[AsCommand(name: 'system:check', description: 'Verifica PostgreSQL e Redis senza esporre credenziali.')]
 final class SystemCheckCommand extends Command
 {
+    public function __construct(private readonly ReadinessCheck $readiness)
+    {
+        parent::__construct();
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $result = (new ReadinessCheck(new ConnectionFactory()))->check();
+        $result = $this->readiness->check();
 
         foreach ($result['components'] as $component => $ready) {
             $output->writeln(sprintf('%s: %s', $component, $ready ? 'OK' : 'KO'));
