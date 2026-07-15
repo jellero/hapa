a3802699bf7dea611dfcf4cdfb30d5e6a9bd6a2e
jellero <j.ellero@gmail.com>
2026-07-15 22:14:08 +0200

    Tipizza il contratto marketplace

--- app/Modules/Marketplace/Contract/MarketplaceAdapter.php
+++ app/Modules/Marketplace/Contract/MarketplaceAdapter.php
@@ -4,20 +4,18 @@ declare(strict_types=1);
 
 namespace Hapa\Modules\Marketplace\Contract;
 
+use Hapa\Modules\Marketplace\Contract\Dto\ExternalOrder;
+use Hapa\Modules\Marketplace\Contract\Dto\ShippingAddress;
+use Hapa\Modules\Marketplace\Contract\Dto\TrackingNotification;
+
 interface MarketplaceAdapter
 {
-    /** @return list<array<string, mixed>> */
+    /** @return list<ExternalOrder> */
     public function importOpenOrders(): array;
 
     public function acceptOrder(string $externalOrderId): void;
 
-    /** @return array<string, mixed>|null */
-    public function fetchShippingAddress(string $externalOrderId): ?array;
+    public function fetchShippingAddress(string $externalOrderId): ?ShippingAddress;
 
-    public function sendTracking(
-        string $externalOrderId,
-        string $carrier,
-        string $trackingNumber,
-        bool $partial,
-    ): void;
+    public function sendTracking(TrackingNotification $notification): void;
 }
