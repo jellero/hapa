11537ffb7144d11f8d218f126a1cf13c18f98928
jellero <j.ellero@gmail.com>
2026-07-16 22:32:54 +0200

    Definisce il repository della inbox RabbitMQ

---
+++ app/Core/Messaging/InboxRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hapa\Core\Messaging;
+
+use DateTimeImmutable;
+
+interface InboxRepository
+{
+    public function begin(MessageEnvelope $message, DateTimeImmutable $receivedAt): ?int;
+
+    public function complete(string $messageId, DateTimeImmutable $processedAt): void;
+
+    public function recordFailure(
+        MessageEnvelope $message,
+        DateTimeImmutable $failedAt,
+        string $error,
+    ): void;
+}
